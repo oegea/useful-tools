c93af9db9711b742ef2adaff3e3e692eee5e6939
Oriol Egea <egea@oriol.im>
2022-06-19 21:56:12 +0200

    tests: Start to work in tests

---
+++ packages/localstorage/src/localstorageSpec.js
@@ -0,0 +1,35 @@
+/** 
+ * This file is part of Useful set of javascript tools.
+ * https://github.com/oegea/useful
+ * 
+ * Copyright (C) 2022 Oriol Egea Carvajal
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import {expect} from 'chai';
+import LocalStorageDatabase from './index.js';
+
+describe('@useful-tools/localstorage library tests', () => {
+
+    
+
+    it('should derive an aes key from a password string', async () => {
+        
+
+        expect(aesKey).to.be.an('CryptoKey');
+    });
+
+
+});
